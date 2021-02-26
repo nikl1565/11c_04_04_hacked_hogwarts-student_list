9f2fdace38b8fb17ed360c2cc9c24d808c8ac0f2
Niklas Vejlby Schjoldager <nikl1565@stud.kea.dk>
2021-02-26 11:16:33 +0100

    Added some functionality to prefetch, visual feedback for chosen filter and sort

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ window.addEventListener("DOMContentLoaded", init);
 const allStudents = [];
 
 const Student = {
-    fullName: "-unknown-",
-    firstName: "-unknown-",
-    middleName: "-unknown-",
-    nickName: undefined,
-    lastName: "-unknown-",
+    fullname: "-unknown-",
+    firstname: "-unknown-",
+    middlename: "-unknown-",
+    nickname: undefined,
+    lastname: "-unknown-",
     gender: undefined,
     house: undefined,
     image: undefined,
@@ -29,6 +29,7 @@ const settings = {
     filterBy: "all",
 };
 
+// TODO: Hopefully not needed
 let unfilteredStudents;
 
 function init() {
@@ -46,7 +47,6 @@ function loadJSON() {
         .then((response) => response.json())
         .then((jsonData) => {
             unfilteredStudents = jsonData;
-            console.log("unfilteredStudents", unfilteredStudents);
 
             prepareObjects(jsonData);
         });
@@ -55,33 +55,32 @@ function loadJSON() {
 // TODO: Clean up
 function prepareObjects(jsonData) {
     console.log("prepareObjects");
-    console.log(jsonData);
 
     jsonData.forEach((jsonObject) => {
         const student = Object.create(Student);
 
-        const fullName = jsonObject.fullname.toLowerCase().trim();
-        const firstSpaceInFullName = fullName.indexOf(" ");
-        const lastSpaceInFullName = fullName.lastIndexOf(" ");
-        const checkForHyphen = fullName.indexOf("-");
+        const fullname = jsonObject.fullname.toLowerCase().trim();
+        const firstSpaceInFullName = fullname.indexOf(" ");
+        const lastSpaceInFullName = fullname.lastIndexOf(" ");
+        const checkForHyphen = fullname.indexOf("-");
 
         let firstName;
         let firstNameFirstLetter;
         // Firstname
         if (firstSpaceInFullName === -1) {
-            firstNameFirstLetter = fullName[0].toUpperCase();
-            const firstNameAfterFirstLetter = fullName.substring(1);
+            firstNameFirstLetter = fullname[0].toUpperCase();
+            const firstNameAfterFirstLetter = fullname.substring(1);
             firstName = firstNameFirstLetter + firstNameAfterFirstLetter;
-            student.firstName = firstNameFirstLetter + firstNameAfterFirstLetter;
+            student.firstname = firstNameFirstLetter + firstNameAfterFirstLetter;
         } else {
-            const findFirstName = fullName.substring(0, firstSpaceInFullName);
+            const findFirstName = fullname.substring(0, firstSpaceInFullName);
             firstNameFirstLetter = findFirstName[0].toUpperCase();
             const firstNameAfterFirstLetter = findFirstName.substring(1);
             firstName = firstNameFirstLetter + firstNameAfterFirstLetter;
-            student.firstName = firstNameFirstLetter + firstNameAfterFirstLetter;
+            student.firstname = firstNameFirstLetter + firstNameAfterFirstLetter;
         }
 
-        let middlePartOfName = fullName.substring(firstSpaceInFullName, lastSpaceInFullName).trim();
+        let middlePartOfName = fullname.substring(firstSpaceInFullName, lastSpaceInFullName).trim();
         // Middlename or nickname exists
         if (middlePartOfName) {
             // If nickname
@@ -90,45 +89,41 @@ function prepareObjects(jsonData) {
 
                 const middlePartOfNameFirstLetter = middlePartOfName[0].toUpperCase();
                 const middlePartOfNameAfterFirstLetter = middlePartOfName.substring(1);
-                student.nickName = middlePartOfNameFirstLetter + middlePartOfNameAfterFirstLetter;
+                student.nickname = middlePartOfNameFirstLetter + middlePartOfNameAfterFirstLetter;
                 student.middleName = null;
 
                 // Else middlename
             } else {
                 const middlePartOfNameFirstLetter = middlePartOfName[0].toUpperCase();
                 const middlePartOfNameAfterFirstLetter = middlePartOfName.substring(1);
-                student.middleName = middlePartOfNameFirstLetter + middlePartOfNameAfterFirstLetter;
-                student.nickName = null;
+                student.middlename = middlePartOfNameFirstLetter + middlePartOfNameAfterFirstLetter;
+                student.nickname = null;
             }
         } else {
-            student.middleName = null;
-            student.nickName = null;
+            student.middlename = null;
+            student.nickname = null;
         }
 
         // Lastname
-        const lastName = fullName.substring(lastSpaceInFullName).trim();
+        const lastName = fullname.substring(lastSpaceInFullName).trim();
         let hasLastNameHyphen;
         let afterHyphenUppercase;
 
         if (firstName.toLowerCase() !== lastName.toLowerCase()) {
-            console.log(lastName);
             hasLastNameHyphen = lastName.indexOf("-");
             // If no hyphen
             if (hasLastNameHyphen === -1) {
                 const lastNameFirstLetter = lastName[0].toUpperCase();
                 const lastNameAfterFirstLetter = lastName.substring(1);
-                student.lastName = lastNameFirstLetter + lastNameAfterFirstLetter;
+                student.lastname = lastNameFirstLetter + lastNameAfterFirstLetter;
                 // Else has hyphen
             } else {
                 const lastNameFirstLetter = lastName[0].toUpperCase();
                 const beforeHyphenUppercase = lastName.substring(1, hasLastNameHyphen + 1);
-                console.log("beforeHyphen", beforeHyphenUppercase);
                 const hyphenUppercase = lastName[hasLastNameHyphen + 1].toUpperCase();
-                console.log("hyphenUppercase", hyphenUppercase);
                 afterHyphenUppercase = lastName.substring(hasLastNameHyphen + 2);
-                console.log("afterHyphenUppercase", afterHyphenUppercase);
 
-                student.lastName = lastNameFirstLetter + beforeHyphenUppercase + hyphenUppercase + afterHyphenUppercase;
+                student.lastname = lastNameFirstLetter + beforeHyphenUppercase + hyphenUppercase + afterHyphenUppercase;
             }
         } else {
         }
@@ -145,8 +140,6 @@ function prepareObjects(jsonData) {
         const genderAfterFirstLetter = gender.substring(1);
         student.gender = genderFirstLetter + genderAfterFirstLetter;
 
-        console.log(student.lastName);
-
         // Image
         let multipleWithTheSameLastName = 0;
         unfilteredStudents.forEach((unfilteredStudent) => {
@@ -156,7 +149,7 @@ function prepareObjects(jsonData) {
                 .trim()
                 .toLowerCase();
 
-            if (lastName === student.lastName.toLowerCase()) {
+            if (lastName === student.lastname.toLowerCase()) {
                 multipleWithTheSameLastName++;
             }
         });
@@ -168,8 +161,6 @@ function prepareObjects(jsonData) {
             student.image = `img/${lastName.substring(hasLastNameHyphen + 1)}_${firstNameFirstLetter.toLowerCase()}.png`;
             // Else normal way
         } else if (multipleWithTheSameLastName >= 2) {
-            console.log("lol");
-            console.log("firstName", firstName);
             student.image = `img/${lastName}_${firstName.toLowerCase()}.png`;
         } else {
             student.image = `img/${lastName}_${firstNameFirstLetter.toLowerCase()}.png`;
@@ -177,8 +168,6 @@ function prepareObjects(jsonData) {
         // Add student to allStudent list
         allStudents.push(student);
     });
-
-    console.table(allStudents);
 
     buildList();
 }
@@ -188,52 +177,53 @@ function buildList() {
     const filteredList = filterList(allStudents);
 
     // TODO: Take the filtered list and sort it
-    // const sortedList = sortList(currentList);
+    const sortedList = sortList(filteredList);
 
     // Display the list
     displayList(sortedList);
 }
 
-function displayList() {
-    console.log("displayListOfStudents");
-
+function displayList(students) {
     // Reset list
     document.querySelector(".js-student-list").innerHTML = "";
 
     // Loop through every student and display them on the screen
-    allStudents.forEach(displayStudent);
+    students.forEach(displayStudent);
 }
 
-function displayStudent() {
-    console.log("displayStudent");
-
+function displayStudent(student) {
     const templateClone = templates.student.cloneNode(true);
 
     // Profile image
     templateClone.querySelector(".c-student__profile-image").src = student.image;
     // First name
-    templateClone.querySelector(".c-student__first-name-cell").textContent = student.firstName;
+    templateClone.querySelector(".c-student__first-name-cell").textContent = student.firstname;
     // Last name
-    templateClone.querySelector(".c-student__last-name-cell").textContent = student.lastName;
+    templateClone.querySelector(".c-student__last-name-cell").textContent = student.lastname;
     // House image
     templateClone.querySelector(".c-student__house-image").src = `img/shield-${student.house.toLowerCase()}.png`;
 
     // Prefect
     templateClone.querySelector(".c-student__prefect-image").src = "img/shield-prefect.png";
-    // If student is prefect
+    // If student is prefect - highlight prefect icon
     if (student.prefect) {
-        templateClone.querySelector(".c-student__prefect-image").classList.remove("u-image-greyscale");
+        templateClone.querySelector(".c-student__prefect-image").classList.add("is-active");
     }
 
     // Squad
     templateClone.querySelector(".c-student__squad-image").src = "img/squad-medal.png";
     // If student is a part of the squad
     if (student.squad) {
-        templateClone.querySelector(".c-student__squad-image").classList.remove("u-image-greyscale");
+        templateClone.querySelector(".c-student__squad-image").classList.add("is-active");
     }
 
     // Show popup if click on student
-    templateClone.querySelector(".c-student").addEventListener("click", clickStudent);
+    templateClone.querySelector(".c-student__profile-image").addEventListener("click", clickStudent);
+
+    templateClone.querySelector("[data-action=prefect]").addEventListener("click", clickPrefectButton);
+    templateClone.querySelector("[data-action=squad]").addEventListener("click", clickSquadButton);
+
+    document.querySelector(".js-student-list").appendChild(templateClone);
 
     function clickStudent() {
         console.log(student);
@@ -292,27 +282,131 @@ function displayStudent() {
         document.querySelector(".js-student-popup-close-button").addEventListener("click", closeStudentPopup);
     }
 
-    document.querySelector(".js-student-list").appendChild(templateClone);
+    function clickPrefectButton() {
+        if (student.prefect) {
+            student.prefect = false;
+        } else {
+            tryToMakeStudentPrefect(student);
+        }
+
+        buildList();
+    }
+
+    function clickSquadButton() {
+        if (student.squad) {
+            student.squad = false;
+        } else {
+            student.squad = true;
+        }
+        console.log(`InQ squad: ${student.squad}`);
+        buildList();
+    }
 }
 
 function initButtons() {
     // Filter buttons
-    document.querySelectorAll("[data-action=filter]").forEach((filterButton) => {
+    const filterButtons = document.querySelectorAll("[data-action=filter]");
+    filterButtons.forEach((filterButton) => {
         filterButton.addEventListener("click", clickFilterButton);
     });
 
     // Sort buttons
-    document.querySelectorAll("[data-action=sort]").forEach((sortButton) => {
+    const sortButtons = document.querySelectorAll("[data-action=sort]");
+    sortButtons.forEach((sortButton) => {
         sortButton.addEventListener("click", clickSortButton);
     });
+
+    function clickFilterButton(event) {
+        const buttonClicked = event.target;
+        const filterClicked = buttonClicked.dataset.filter;
+
+        filterButtons.forEach((filterButton) => filterButton.classList.remove("is-active"));
+        buttonClicked.classList.add("is-active");
+
+        setFilter(filterClicked);
+    }
+
+    function clickSortButton(event) {
+        const buttonClicked = event.target;
+        const sortClicked = buttonClicked.dataset.sort;
+        const sortDirClicked = buttonClicked.dataset.sortDirection;
+
+        sortButtons.forEach((sortButton) => sortButton.classList.remove("is-active"));
+        buttonClicked.classList.add("is-active");
+
+        if (sortDirClicked === "asc") {
+            event.target.dataset.sortDirection = "desc";
+        } else {
+            event.target.dataset.sortDirection = "asc";
+        }
+
+        setSort(sortClicked, sortDirClicked);
+    }
 }
 
-function clickFilterButton(event) {
-    const buttonClicked = event.target.dataset.filter;
-    console.log(`Sort by: ${buttonClicked}`);
+function setFilter(filter) {
+    // Update the filter
+    settings.filterBy = filter;
+
+    // Build the list again with the updated filter
+    buildList();
 }
 
-function clickSortButton(event) {
-    const buttonClicked = event.target.dataset.sort;
-    console.log(`Sort by: ${buttonClicked}`);
+function filterList(filteredList) {
+    filteredList = allStudents.filter(filterByProperty);
+
+    return filteredList;
+}
+
+function filterByProperty(student) {
+    if (settings.filterBy === "all") {
+        return true;
+    } else {
+        return student.house.toLowerCase() === settings.filterBy;
+    }
+}
+
+function setSort(sortBy, sortDir) {
+    settings.sortBy = sortBy;
+    settings.sortDir = sortDir;
+    console.log(sortBy);
+
+    buildList();
+}
+
+function sortList(sortedList) {
+    let direction = 1;
+
+    if (settings.sortDir === "desc") {
+        direction = -1;
+    } else {
+        direction = 1;
+    }
+
+    sortedList = sortedList.sort(sortByProperty);
+
+    function sortByProperty(studentA, studentB) {
+        if (studentA[settings.sortBy] < studentB[settings.sortBy]) {
+            return -1 * direction;
+        } else {
+            return 1 * direction;
+        }
+    }
+
+    return sortedList;
+}
+
+function tryToMakeStudentPrefect(selectedStudent) {
+    console.log(selectedStudent);
+    const prefectsFromHouse = allStudents.filter((student) => student.house === selectedStudent.house && student.prefect);
+    console.table(prefectsFromHouse);
+    const numberOfPrefects = prefectsFromHouse.length;
+
+    if (numberOfPrefects >= 2) {
+        console.log("There can only be two prefects for each house!");
+        // TODO: Add modal to remove either A, B or do nothing
+    } else {
+        console.log("Student added");
+        selectedStudent.prefect = true;
+    }
 }
